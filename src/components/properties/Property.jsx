b9ac6d3fc7a495168e875466442c521146563297
Sachithra Pinnaduwa <136422527+SachithraPinnaduwa@users.noreply.github.com>
2023-12-18 12:26:06 +0530

    JASON

--- src/components/properties/Property.jsx
+++ src/components/properties/Property.jsx
@@ -11,35 +11,42 @@ export default function Property() {
   const [pressed, setPressed] = useState(false);
   return (
     <div className="grid lg:grid-flow-col border-8 border-teal-500 md:grid-flow-row bg-slate-100 m-5 rounded-xl">
-      <div className="mx-auto my-auto">
+      <div className="mx-auto my-auto ml-5">
         <img src={house} className="w-[600px]" alt="house" />
       </div>
-      <div>
+      <div className="mx-10">
         <div className="flex flex-row ml-10 justify-center mt-4">
           <h1 className="ml-5">Property Name</h1>
           <h1 className="ml-5">Property Location</h1>
         </div>
-        <p className="ml-10 mt-5">
-          Located on the modern development of Valley Mews is this second floor
-          apartment. There is open plan living accommodation with Juliette
-          balcony looking out to surrounding Saddleworth countryside and modern
-          shower room. Internally accessed from a communal hallway via stairs
-          into the apartment hallway which has an intercom entry system. There
-          is an open plan lounge through kitchen, two bedrooms and shower room.
-        </p>
+
+        <div className="min-h-[120px]">
+          <p className=" mt-5">
+            Just Property are delighted to bring to the market an imposing and
+            substantial six bedroom Victorian semi-detached house which is
+            arranged over three floors and is brimming with beautiful period
+            features. The property enjoys a stunning position in the heart of
+            Bexhill’s picturesque Old Town and whilst the house does require
+            updating there is huge potential for any new owner to create a
+            magnificent home to suit their needs.
+          </p>
+        </div>
+
         <h1 className="ml-10 mt-5 mb-6">Property Price</h1>
         <div>
-
           <div className="flex flex-row float-right mb-10">
-          <Link to="/search/1" className="text-3xl font-bold text-emerald-500 ">
-          <button
-              type="submit"
-              className="flex-none rounded-md bg-teal-500 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-600 mx-10"
+            <Link
+              to="/search/1"
+              className="text-3xl font-bold text-emerald-500 "
             >
-              Visit
-            </button>
-         </Link>
-          
+              <button
+                type="submit"
+                className="flex-none rounded-md bg-teal-500 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-600 mx-10"
+              >
+                Visit
+              </button>
+            </Link>
+
             <button
               onClick={() => {
                 setPressed(!pressed);
@@ -59,8 +66,6 @@ export default function Property() {
               )}
             </button>
           </div>
-
-
         </div>
       </div>
     </div>
